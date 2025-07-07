a71438aa04badd2e8be44a98899812a45dd44a1b
Svetlozar Trifonov <svetlozartrifonov60@gmail.com>
2025-07-07 19:53:37 +0300

    Product_Create_View_Model added.

---
+++ BidCommerce/Models/ViewModels/ProductCreateViewModel.cs
@@ -0,0 +1,10 @@
+﻿using BidCommerce.Models;
+
+namespace BidCommerce.ViewModels
+{
+    public class ProductCreateViewModel
+    {
+        public Product Product { get; set; } = new Product();
+        public IEnumerable<Category> Categories { get; set; } = new List<Category>();
+    }
+}
