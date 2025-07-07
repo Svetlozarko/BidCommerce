cca81397fe2a376391f99f04776b40de9b3400a9
Svetlozar Trifonov <svetlozartrifonov60@gmail.com>
2025-07-07 19:54:36 +0300

    Product_Index_View_Model added.

---
+++ BidCommerce/Models/ViewModels/ProductIndexViewModel.cs
@@ -0,0 +1,15 @@
+﻿using BidCommerce.Models;
+
+namespace BidCommerce.ViewModels
+{
+    public class ProductIndexViewModel
+    {
+        public IEnumerable<Product> Products { get; set; } = new List<Product>();
+        public IEnumerable<Category> Categories { get; set; } = new List<Category>();
+        public int? SelectedCategoryId { get; set; }
+        public decimal? MinPrice { get; set; }
+        public decimal? MaxPrice { get; set; }
+        public string? SortBy { get; set; } = "newest";
+        public string? ListingType { get; set; }
+    }
+}
