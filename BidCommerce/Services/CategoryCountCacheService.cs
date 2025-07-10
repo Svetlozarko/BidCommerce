815ace707eba2ab491c84efcdf0013ea13fdf56a
Svetlozar Trifonov <svetlozartrifonov60@gmail.com>
2025-07-10 13:11:02 +0300

    Redis service.

---
+++ BidCommerce/Services/CategoryCountCacheService.cs
@@ -0,0 +1,27 @@
+﻿using BidCommerce.Interfaces;
+using StackExchange.Redis;
+
+namespace BidCommerce.Services
+{
+    public class CategoryCountCacheService : ICategoryCountCacheService
+    {
+        private readonly IDatabase _redisDb;
+
+        public CategoryCountCacheService(IConnectionMultiplexer connection)
+        {
+            _redisDb = connection.GetDatabase();
+        }
+
+        public async Task<int?> GetCategoryCountAsync(string category)
+        {
+            var value = await _redisDb.StringGetAsync($"category_count:{category}");
+            return value.HasValue ? (int?)int.Parse(value!) : null;
+        }
+
+        public async Task SetCategoryCountAsync(string category, int count, TimeSpan? expiry = null)
+        {
+            await _redisDb.StringSetAsync($"category_count:{category}", count, expiry);
+        }
+    }
+
+}
