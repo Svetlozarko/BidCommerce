03435dbc2a412c4ad09ce56234de9d40d15c0934
Svetlozar Trifonov <svetlozartrifonov60@gmail.com>
2025-07-13 19:33:34 +0300

    Dashboard viewmodel added.

---
+++ BidCommerce/Models/ViewModels/MyDashboardViewModel.cs
@@ -0,0 +1,18 @@
+﻿using BidCommerce.Data;
+using BidCommerce.Models;
+using System.Collections.Generic;
+
+namespace BidCommerce.ViewModels
+{
+    public class MyDashboardViewModel
+    {
+        public ApplicationUser CurrentUser { get; set; }
+        public List<Product> ActiveListings { get; set; } = new List<Product>();
+        public List<Product> PastListings { get; set; } = new List<Product>();
+        public decimal TotalRevenue { get; set; }
+        public int ActiveListingsCount { get; set; }
+        public decimal ThisMonthRevenue { get; set; } = 0;
+        public double Rating { get; set; } = 0.0;
+        public int FollowersCount { get; set; } = 0;
+    }
+}
