616126ae8d07698e30e0677f664b53ee9a6f6922
Svetlozar Trifonov <svetlozartrifonov60@gmail.com>
2025-07-28 22:48:25 +0300

    Follower model added.

---
+++ BidCommerce/Models/Follower.cs
@@ -0,0 +1,16 @@
+﻿using BidCommerce.Data;
+
+namespace BidCommerce.Models
+{
+    public class Follower
+    {
+        public string FollowerId { get; set; }
+        public ApplicationUser FollowerUser { get; set; }
+
+        public string FollowedId { get; set; }
+        public ApplicationUser FollowedUser { get; set; }
+
+        public DateTime FollowedAt { get; set; } = DateTime.UtcNow;
+    }
+
+}
