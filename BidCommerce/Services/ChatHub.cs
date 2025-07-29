223fc9891c44a19995ce5d6390e0c66ad9dcd4e7
Svetlozar Trifonov <svetlozartrifonov60@gmail.com>
2025-07-29 22:51:12 +0300

    Chat service added.

---
+++ BidCommerce/Services/ChatHub.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace BidCommerce.Services
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string recipientId, string message)
+        {
+            var senderId = Context.UserIdentifier;
+
+            await Clients.User(recipientId).SendAsync("ReceiveMessage", senderId, message);
+        }
+    }
+
+}
