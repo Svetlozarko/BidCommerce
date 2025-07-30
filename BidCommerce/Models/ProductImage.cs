13b96486c7bc453c86a27247927ca2f86bd21d83
Svetlozar Trifonov <svetlozartrifonov60@gmail.com>
2025-07-30 17:56:21 +0300

    ProductImage model added.

--- BidCommerce/Models/ProductImage.cs
+++ BidCommerce/Models/ProductImage.cs
@@ -1,15 +1,21 @@
-﻿namespace BidCommerce.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BidCommerce.Models
 {
     public class ProductImage
     {
         public int Id { get; set; }
 
         public int ProductId { get; set; }
+
+        [ForeignKey("ProductId")]
         public virtual Product Product { get; set; }
 
         public string ImageUrl { get; set; }
 
         public bool IsPrimary { get; set; }
+        public int? DisplayOrder { get; set; } // Optional: for ordering images
+
     }
 
 }
