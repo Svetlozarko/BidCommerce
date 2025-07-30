9aadb1c05bac6c76479e8392f9657a74ea668113
Svetlozar Trifonov <svetlozartrifonov60@gmail.com>
2025-07-30 16:32:45 +0300

    ImageFile field changed to accept multiple images.

--- BidCommerce/Models/ViewModels/ProductCreateViewModel.cs
+++ BidCommerce/Models/ViewModels/ProductCreateViewModel.cs
@@ -6,7 +6,7 @@ namespace BidCommerce.ViewModels
     {
         public Product Product { get; set; } = new Product();
         public IEnumerable<Category> Categories { get; set; } = new List<Category>();
-        public IFormFile ImageFile { get; set; }
+        public List<IFormFile> ImageFiles { get; set; } = new();
         public string? AuctionDuration { get; set; } // e.g., "1 hour", "1 day", "3 days", etc.
         public IEnumerable<Condition> Condition { get; set; } = new List<Condition>();
 
