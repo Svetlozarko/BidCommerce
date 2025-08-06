769a875e0198281b60128871f90f45fe80043ed5
Svetlozar Trifonov <svetlozartrifonov60@gmail.com>
2025-08-06 19:09:11 +0300

    Added Interface for the search keywords caching.

---
+++ BidCommerce/Interfaces/ISearchableTextRedis.cs
@@ -0,0 +1,10 @@
+﻿namespace BidCommerce.Interfaces
+{
+    public interface ISearchableTextRedis
+    {
+        Task SaveProductAsync(int productId, string searchableText);
+        Task<string?> GetProductAsync(int productId);
+        Task DeleteProductAsync(int productId);
+
+    }
+}
