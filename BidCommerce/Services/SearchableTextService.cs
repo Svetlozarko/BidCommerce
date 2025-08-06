ddbe107839a5dd1bc2c708574a6157347b730777
Svetlozar Trifonov <svetlozartrifonov60@gmail.com>
2025-08-06 19:08:41 +0300

    Added service for caching the search text keywords.

---
+++ BidCommerce/Services/SearchableTextService.cs
@@ -0,0 +1,40 @@
+﻿using StackExchange.Redis;
+using BidCommerce.Interfaces;
+
+namespace BidCommerce.Services
+{
+    public class SearchableTextService : ISearchableTextRedis
+    {
+        private readonly IDatabase _redis;
+        private readonly ILogger<SearchableTextService> _logger;
+
+        public SearchableTextService(IConnectionMultiplexer redis, ILogger<SearchableTextService> logger)
+        {
+            _redis = redis.GetDatabase();
+            _logger = logger;
+        }
+
+        public async Task SaveProductAsync(int productId, string searchableText)
+        {
+            string cleanedText = searchableText
+                .Replace("\r", " ")
+                .Replace("\n", " ");
+            string key = $"product:{productId}";
+            await _redis.StringSetAsync(key, cleanedText);
+        }
+
+
+        public async Task<string?> GetProductAsync(int productId)
+        {
+            string key = $"product:{productId}";
+            return await _redis.StringGetAsync(key);
+        }
+
+        public async Task DeleteProductAsync(int productId)
+        {
+            string key = $"product:{productId}";
+            await _redis.KeyDeleteAsync(key);
+        }
+    }
+
+}
